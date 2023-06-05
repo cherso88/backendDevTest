077abba7c137125c98773c1fb5e6dc5311d5be61
cherso88 <cersosimof88@gmail.com>
2023-06-05 13:14:37 +0200

    fix productNotFoundException

--- src/main/java/com/test/zara/exception/ProductNotFoundException.java
+++ src/main/java/com/test/zara/exception/ProductNotFoundException.java
@@ -1,15 +1,6 @@
 package com.test.zara.exception;
 
 public class ProductNotFoundException extends Exception {
-
-    public ProductNotFoundException(String message, int value) {
-        super(message);
-    }
-
-    public ProductNotFoundException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-
     public ProductNotFoundException(String s) {
         super(s);
     }
